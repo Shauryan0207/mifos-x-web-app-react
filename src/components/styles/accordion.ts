bb359fce4d3833c6c38d3b7e59d6b85657f04b3a
shubhamsharma9199 <shubham.24bcs10320@sst.scaler.com>
2026-01-19 21:57:30 +0530

    MXWAR-34:chore: add MPL 2.0 license header to all source files

--- src/components/styles/accordion.ts
+++ src/components/styles/accordion.ts
@@ -1,1 +1,8 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 export const accordionRootClass="focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180"
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import MfNavbar from "@/components/custom/navbar/MfNavbar"
 import { AppSidebar } from "@/components/custom/sidebar/AppSidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
